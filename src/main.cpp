a3092b07ab775aa5effe2c7cd0ef92e9b9613e21
likes linux but uses windows <46166499+SBSProgrammer@users.noreply.github.com>
2020-11-21 21:59:52 +0100

    Make the code use variables from constants.h
    Basically, there's a Constants.h file, but it isn't actually used, so I made it so that main.cpp uses those values instead of defining them in the file.

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,7 @@
 #include <cmath>
 #include "Renderer.h"
 #include <fstream>
-
-constexpr float PI = 3.14159;
-constexpr float SPEED = 2.0;
-constexpr int FOV = 60;
-
-constexpr int MAP_SIZE = 20;
-constexpr int TILE_SIZE = 64;
-constexpr int MINIMAP_SCALE = 4;
-constexpr int MINIMAP_TILE_SIZE = TILE_SIZE / MINIMAP_SCALE;
-constexpr int WINDOW_WIDTH = 1280;
-constexpr int WINDOW_HEIGHT = 720;
+#include "Constants.h"
 
 float rads(float degs)
 {
